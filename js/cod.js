08c05e95fa38bac1dc751d420a30461dd7b990c7
rod_hen@hotmail.com <rod_hen@hotmail.com>
2020-02-10 13:31:44 -0300

    add js

--- js/cod.js
+++ js/cod.js
@@ -8,5 +8,19 @@ var myVar = setInterval(myTimer, 1000);
 
 function chanceColor(color){
     document.getElementById("home").style.background = color;
+    document.getElementById("myNav").style.background = color;
 } 
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myNav").style.top = "0";
+    document.getElementById("myNav").style.transition = "all 0.3s ease";
+  } else {
+    document.getElementById("myNav").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
                 
